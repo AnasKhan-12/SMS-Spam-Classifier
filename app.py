1d436a1a746f4887c1734f79189c810cad4f837b
Anas Khan <anskhan67@gmail.com>
2025-06-20 16:29:23 +0500

    initial commit

---
+++ app.py
@@ -0,0 +1,71 @@
+from flask import Flask, request, render_template
+import pickle
+import nltk
+import string
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+nltk.download('punkt')       # Tokenizer
+nltk.download('stopwords')   # Stopword list
+
+stemmer = PorterStemmer()
+
+def transform(text):
+    text = text.lower() 
+    text = nltk.word_tokenize(text)
+
+    y = []
+    for i in text:
+        if i.isalnum():  # Only letters and numbers
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:  # Remove stopwords and punctuation
+        if i not in stopwords.words("english") and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+    
+    for i in text:
+        y.append(stemmer.stem(i))
+
+    return " ".join(y)
+def predict_sms(msg):
+    cleaned = transform(msg)
+    vect_msg = vectorizer.transform([cleaned])
+    result = model.predict(vect_msg)[0]
+    return 'Spam' if result == 1 else 'Not Spam'
+
+
+app = Flask(__name__)
+
+# Load model and vectorizer
+model = pickle.load(open('model.pkl', 'rb'))
+vectorizer = pickle.load(open('vectorizer.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        message = request.form['message']
+        result = predict_sms(message)
+        print("USER INPUT:", message)
+        print("PREDICTION:", result)
+        return render_template('index.html', prediction_text=result)
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
